7b4a00c16eb3f7e5779bfed7cf10384feb721a63
Artur <arthar@mail.ru>
2018-06-13 21:31:21 +0400

    Add files via upload

---
+++ exclude home page from search wp.php
@@ -0,0 +1,11 @@
+// Exclude pages from WordPress Search
+// add the following code to your theme’s functions.php file or a site-specific plugin. 
+if (!is_admin()) {
+function wpb_search_filter($query) {
+if ($query->is_search) {
+$query->set('post_type', 'post');
+}
+return $query;
+}
+add_filter('pre_get_posts','wpb_search_filter');
+}
